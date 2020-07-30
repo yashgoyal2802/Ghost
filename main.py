22e8492dda71e6dcd0e5d6d1f9b7d4c795393ee9
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-07-30 16:20:38 +0530

    date function

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ def speak(audio):
 def time():
     now = datetime.now().strftime("%I:%M:%S")
     speak(now)
+
+
+def date():
+    year = int(datetime.now().year)
+    month = int(datetime.now().month)
+    date = int(datetime.now().day)
+    speak(date)
+    speak(month)
+    speak(year)
