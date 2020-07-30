c84a42c1a1e8791d7924164ad2a98afdce238f70
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-07-30 16:21:24 +0530

    wishme function

--- main.py
+++ main.py
@@ -21,3 +21,21 @@ def date():
     speak(date)
     speak(month)
     speak(year)
+
+
+def wishme():
+    speak("Welcome Back Sir!")
+    speak("Its")
+    time()
+    speak("and today is")
+    date()
+    hour = datetime.now().hour
+    if hour >= 6 and hour < 12:
+        speak("Good Morning Sir!")
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon Sir!")
+    elif hour >= 18 and hour < 24:
+        speak("Good Evening Sir!")
+    else:
+        speak("Good Night Sir!")
+    speak("This is GHOST, How may I help you ?")
